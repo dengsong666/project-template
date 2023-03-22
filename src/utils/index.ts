b3a83bd85cfbd85dfca2ab25b9c40b5cbe702adc
dengsong666 <dengsong2022@gmail.com>
2023-03-22 17:19:26 +0800

    feat

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,9 @@
 export * from './common'
+declare global {
+  interface String {
+    case(this: string): string
+  }
+}
+String.prototype.case = function (upper = true) {
+  return this.replace(/\S/, (s) => (upper ? s.toUpperCase() : s.toLowerCase()))
+}
